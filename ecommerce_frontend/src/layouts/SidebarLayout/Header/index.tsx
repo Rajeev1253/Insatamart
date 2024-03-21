c748cd04811742bb2dcbeafb26d94cad01bac98e
RajeevGoyal21202 <rajeev.1149@zenmonk.tech>
2024-03-21 18:40:23 +0530

    date set

--- ecommerce_frontend/src/layouts/SidebarLayout/Header/index.tsx
+++ ecommerce_frontend/src/layouts/SidebarLayout/Header/index.tsx
@@ -1,46 +1,65 @@
-import { Avatar, Stack, Typography } from '@mui/material'
-import React from 'react'
-import Search from '../../../components/Search'
-import calendar from "../../../assets/svg/Calendar.svg"
-import NotificationsIcon from '@mui/icons-material/Notifications';
+import { Avatar, Stack, Typography } from "@mui/material";
+import React from "react";
+import Search from "../../../components/Search";
+import calendar from "../../../assets/svg/Calendar.svg";
+import NotificationsIcon from "@mui/icons-material/Notifications";
 
 const Headers = () => {
+    const currentDate = new Date().toUTCString().split(" ").slice(0,4).join(" ");
   return (
     <div>
-        <Stack height={"90px"} sx={{bgcolor:"white"}} justifyContent={"center"}  >
-            <Stack justifyContent="space-between" direction={"row"}>
-                <Stack justifyContent={"flex-start"}>
-
-            <Search/> 
-                </Stack>
-            <Stack width="600px"  justifyContent={"center"} direction={"row"} alignItems={"center"}>
-                
-                    <img src={calendar} style={{width:"16px",height:"16px"}}></img>
-                    <Typography fontWeight={600}sx={{ml:1}}>
-                        Monday,4th  September
+      <Stack
+        height={"90px"}
+        sx={{ bgcolor: "white" }}
+        justifyContent={"center"}
+      >
+        <Stack justifyContent="space-between" direction={"row"}>
+          <Stack justifyContent={"flex-start"}>
+            <Search />
+          </Stack>
+          <Stack
+            width="600px"
+            justifyContent={"center"}
+            direction={"row"}
+            alignItems={"center"}
+            gap={"60px"}
+          >
+            <Stack
+              width="470px"
+              justifyContent={"center"}
+              direction={"row"}
+              alignItems={"center"}
+            >
+              <img
+                src={calendar}
+                style={{ width: "16px", height: "16px" }}
+              ></img>
+              <Typography fontWeight={600}sx={{ml:1}}>
+                        {currentDate}
                     </Typography>
-                    <NotificationsIcon sx={{ml:2}}/>
-            <Stack width={"300px"} direction={"row"}  justifyContent={"flex-end"} >
-                <Stack sx={{mr:2}}>
-                    
-                <Typography fontSize={"15px"} fontWeight={"600"}>Mark Collins</Typography>
-                <Typography fontSize={"10px"} color={"gray"}> Business Man</Typography>
-                </Stack>
-                <Avatar/>
-            
+              <Typography fontWeight={600} sx={{ ml: 1 }}>
+                {}
+              </Typography>
+              <NotificationsIcon sx={{ ml: 2 }} />
             </Stack>
+
+            <Stack width={"300px"} direction={"row"}>
+              <Stack sx={{ mr: 1 }}>
+                <Typography fontSize={"15px"} fontWeight={"600"}>
+                  Mark Collins
+                </Typography>
+                <Typography fontSize={"10px"} color={"gray"}>
+                  {" "}
+                  Business Man
+                </Typography>
+              </Stack>
+              <Avatar />
             </Stack>
-        
-
-
-            </Stack>
-            
-
-            
-
+          </Stack>
         </Stack>
+      </Stack>
     </div>
-  )
-}
+  );
+};
 
-export default Headers
+export default Headers;
